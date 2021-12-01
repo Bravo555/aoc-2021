90cd48a06e68b0724b1403db987cd557ef5b3bbb
Marcel Guzik <mguzik555@gmail.com>
2021-12-01 10:47:22 +0100

    Day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+fn main() {
+    let input: Vec<i32> = fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .flat_map(|line| line.parse().ok())
+        .collect();
+
+    let increased = zad2(&input);
+
+    println!("increased: {}", increased);
+}
+
+fn zad1(mut input: impl Iterator<Item = i32>) -> i32 {
+    let mut increased = 0;
+    let mut prev = input.next().unwrap();
+    for elev in input {
+        if elev > prev {
+            increased += 1;
+        }
+        prev = elev;
+    }
+
+    increased
+}
+
+fn zad2(input: &[i32]) -> i32 {
+    let s = input.windows(3).map(|window| window.iter().sum());
+    zad1(s)
+}
